44979c0151ae66645255c33041c36aa0f896c314
Mateusz Cieciura <mateusz5000@gmail.com>
2015-07-05 18:03:34 +0200

    add powershell profile

---
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,15 @@
+﻿Set-Alias g git
+Set-Alias np notepad++
+
+function prompt
+{
+    $location = "$(Get-Location)"
+    $time = "[$(Get-Date -format T)] "
+    
+    Write-Host $time -NoNewline -ForegroundColor Green
+    Write-Host $location -BackgroundColor DarkMagenta -ForegroundColor Black
+    Write-Host ">" -NoNewline
+    return " "
+
+    # "PS " + $(get-location) + " [$(Get-Date -format T)]> "
+}
